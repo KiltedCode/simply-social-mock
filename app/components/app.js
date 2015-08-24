783b07ea94adce7787bcc4c5e85aca0a73a3dc9b
RHGeek <john.niedzwiecki.ii@gmail.com>
2015-08-23 22:10:46 -0400

    Added Relative Timestamps
    Added relative timestamps to the post.
    Leverages angular-moment and moment.js
    Formatted to drop ' ago' and use single letter (i.e. s instead of
    seconds).

--- app/components/app.js
+++ app/components/app.js
@@ -1,10 +1,28 @@
 'use strict';
 
-angular.module('ssm', ['ngRoute', 'ssm.home', 'ssm.user'])
+angular.module('ssm', ['ngRoute', 'ssm.home', 'ssm.user', 'angularMoment'])
 .run(['$rootScope', 'CurrentUserService', function($rootScope, CurrentUserService) {
 	$rootScope.currentUser = CurrentUserService.getCurrentUser();
 	$rootScope.nav = {};
 	$rootScope.nav.showNewMsg = false;
+
+	moment.lang('en', {
+	    relativeTime : {
+	        future: "in %s",
+	        past:   "%s",
+	        s:  "seconds ago",
+	        m:  "1m",
+	        mm: "%dm",
+	        h:  "1h",
+	        hh: "%dh",
+	        d:  "1d",
+	        dd: "%dd",
+	        M:  "1m",
+	        MM: "%dm",
+	        y:  "1y",
+	        yy: "%dy"
+	    }
+	});
 }]);
 
 angular.module('ssm')
--- app/components/ssm.home/ssm.home.ctrls.js
+++ app/components/ssm.home/ssm.home.ctrls.js
@@ -9,6 +9,7 @@ angular.module('ssm.home')
 	$scope.model.newMsg = '';
 	var id = 100;
 
+	/* Adds message from modal to feed */
 	$scope.model.addMsg = function() {
 		if($scope.model.newMsg && $scope.model.newMsg.length > 0) {
 			var newPost = {
@@ -27,9 +28,27 @@ angular.module('ssm.home')
 		}
 	}
 
+	/* adjusts the list of posts to have timestamps relative to run. */
+	var adjustTStoRecent = function() {
+		var lastTS = (new Date()).getTime();
+		for(var i = 0, len = $scope.model.posts.length; i < len; i++) {
+			var rand = 0;
+			if(i <= 5) {
+				rand = Math.floor(Math.random()*10) + 1;
+			} else {
+				rand = Math.floor(Math.random()*36) + 1;
+			}
+
+			console.log('rand', rand);
+			lastTS -= rand * 60000;
+			$scope.model.posts[i].ts = lastTS;
+		}
+	};
+
 	HomeService.getPosts()
 		.success(function(data, status) {
 			$scope.model.posts = data;
+			adjustTStoRecent();
 			console.log('posts', $scope.model.posts);
 		})
 		.error(function(data, status) {
